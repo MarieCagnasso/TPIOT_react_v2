0beaab90d668e73ab2fc26849d0bea87c60fd499
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2021-12-10 17:13:52 +0100

    style de l’affichage du rythme cardiaque basé sur la valeur.

--- src/components/heartRateLive.js
+++ src/components/heartRateLive.js
@@ -1,8 +1,17 @@
 function heartRateLive() {
-    const bpm = Math.floor(Math.random()*160) + 40;
-    return (<div>
+    const bpm = Math.floor(Math.random() * 160) + 40;
+    let classBpm = 'bpmNormal';
+
+    if (bpm > 40 && bpm <= 70) {
+        classBpm = 'bpmHigh';
+    }
+    if (bpm > 140 && bpm <= 200) {
+        classBpm = 'bpmLow';
+    }
+
+    return (<p className={classBpm}>
             Rythme cardiaque :  {bpm}
-        </div>
+        </p>
     )
 }
 
